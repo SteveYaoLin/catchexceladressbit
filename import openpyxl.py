deabdf88b63196e7403fb6bfc2ca5f7f87b318b1
YaoLin <XJ0061@hpcrd.com>
2023-06-24 23:14:12 +0800

    l文件第一行是“bit”信息，第一列是“偏移量”信息。其余为“SIGNAL”信息。每个非空的“SIGNAL”都对应唯一的“bit”信息和唯一的“偏移量”信息。请帮我写一个python程序，可以读取excel的所有“SIGNAL”信息。并在其后输出2个空格后排列“偏移量”信息，再隔2个空格后排列“bit”信息。以上信息以TXT格式输出。excel文件的地址作为端口，运行该程序，可以自行浏览选择excel文件。 <XJ0061@hpcrd.com>

---
+++ import openpyxl.py
@@ -0,0 +1,46 @@
+import openpyxl
+
+def extract_signal_information(filepath):
+    workbook = openpyxl.load_workbook(filepath)
+    sheet = workbook.active
+
+    # 读取SIGNAL、偏移量和bit信息
+    signal_info = []
+    offset_info = []
+    bit_info = []
+
+    for row in sheet.iter_rows(values_only=True):
+        signal = row[0]
+        offset = row[1]
+        bit = row[2]
+
+        # 跳过第一行（标题行）
+        if signal == "SIGNAL":
+            continue
+
+        # 将非空的SIGNAL信息存入列表
+        if signal:
+            signal_info.append(signal)
+            offset_info.append(offset)
+            bit_info.append(bit)
+
+    return signal_info, offset_info, bit_info
+
+def save_information_to_txt(signal_info, offset_info, bit_info, output_filepath):
+    with open(output_filepath, 'w') as output_file:
+        for signal, offset, bit in zip(signal_info, offset_info, bit_info):
+            output_file.write(f"{signal}\t\t{offset}\t\t{bit}\n")
+
+    print(f"信息已保存到：{output_filepath}")
+
+# 获取用户输入的Excel文件路径
+excel_filepath = input("请输入Excel文件路径：")
+
+# 提取信息
+signal_info, offset_info, bit_info = extract_signal_information(excel_filepath)
+
+# 指定输出文件路径
+output_filepath = 'output.txt'
+
+# 保存信息到TXT文件
+save_information_to_txt(signal_info, offset_info, bit_info, output_filepath)
