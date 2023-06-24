82ee8f8bda4191d4a5215f5b888d8330c44af367
SteveYaoLin <yaolin0616@163.com>
2023-06-24 23:29:10 +0800

    执行此代码的结果只抓取了前三列，我要求抓取33列,2次修改可以执行。
    Signed-off-by: SteveYaoLin <yaolin0616@163.com>

--- import openpyxl.py
+++ import openpyxl.py
@@ -4,6 +4,9 @@ def extract_signal_information(filepath):
     workbook = openpyxl.load_workbook(filepath)
     sheet = workbook.active
 
+    # 获取列数
+    num_cols = sheet.max_column
+
     # 读取SIGNAL、偏移量和bit信息
     signal_info = []
     offset_info = []
@@ -12,7 +15,7 @@ def extract_signal_information(filepath):
     for row in sheet.iter_rows(values_only=True):
         signal = row[0]
         offset = row[1]
-        bit = row[2]
+        bits = row[2:num_cols]
 
         # 跳过第一行（标题行）
         if signal == "SIGNAL":
@@ -22,14 +25,15 @@ def extract_signal_information(filepath):
         if signal:
             signal_info.append(signal)
             offset_info.append(offset)
-            bit_info.append(bit)
+            bit_info.append(bits)
 
     return signal_info, offset_info, bit_info
 
 def save_information_to_txt(signal_info, offset_info, bit_info, output_filepath):
     with open(output_filepath, 'w') as output_file:
-        for signal, offset, bit in zip(signal_info, offset_info, bit_info):
-            output_file.write(f"{signal}\t\t{offset}\t\t{bit}\n")
+        for signal, offset, bits in zip(signal_info, offset_info, bit_info):
+            bit_string = '\t\t'.join(str(bit) for bit in bits)
+            output_file.write(f"{signal}\t\t{offset}\t\t{bit_string}\n")
 
     print(f"信息已保存到：{output_filepath}")
 
