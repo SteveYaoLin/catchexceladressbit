1da013a3be0b6ec1baf4558901426702b8929a4d
SteveYaoLin <yaolin0616@163.com>
2023-06-25 11:27:41 +0800

    我举例说明吧。在第2列到第33列和第2行到第5行之间的非零元素，比如USB0_DISABLE，对应的本行里A列为地址数，也就是地址偏移量为10，对应的本列第1行为BIT数，即BIT数为31，那么USB0_DISABLE对应在OUTPUT.txt里显示的就是USB0_DISABLE  10  31。请按照我所描述的例子，更改程序。
    Signed-off-by: SteveYaoLin <yaolin0616@163.com>

--- import openpyxl.py
+++ import openpyxl.py
@@ -4,33 +4,31 @@ def extract_signal_information(filepath):
     workbook = openpyxl.load_workbook(filepath)
     sheet = workbook.active
 
-    # 读取SIGNAL、bit和行数信息
+    # 读取非零元素的信息
     signal_info = []
+    offset_info = []
     bit_info = []
-    row_info = []
 
-    for row in sheet.iter_rows(values_only=True):
-        signal = row[0]
-        bits = row[1:32]  # 假设bit信息在第2列到第33列之间
-        row_num = row[32]  # 假设行数信息在第33列
+    for row in range(1, 5):  # 假设地址偏移量在第2行到第5行之间
+        for col in range(1, 34):  # 假设bit数在第2列到第33列之间
+            signal = sheet.cell(row=row, column=col).value
+            offset = sheet.cell(row=row, column=1).value
+            bit = sheet.cell(row=1, column=col).value
 
-        # 跳过第一行（标题行）和空行
-        if signal == "SIGNAL" or not any(bits):
-            continue
+            # 跳过空单元格和标题行
+            if signal is None or signal == "SIGNAL":
+                continue
 
-        # 添加非空元素的信息
-        for bit in bits:
-            if bit:
-                signal_info.append(signal)
-                bit_info.append(bit)
-                row_info.append(row_num)
+            signal_info.append(signal)
+            offset_info.append(offset)
+            bit_info.append(bit)
 
-    return signal_info, bit_info, row_info
+    return signal_info, offset_info, bit_info
 
-def save_information_to_txt(signal_info, bit_info, row_info, output_filepath):
+def save_information_to_txt(signal_info, offset_info, bit_info, output_filepath):
     with open(output_filepath, 'w') as output_file:
-        for signal, bit, row in zip(signal_info, bit_info, row_info):
-            output_file.write(f"{signal}\t\t{bit}\t\t{row}\n")
+        for signal, offset, bit in zip(signal_info, offset_info, bit_info):
+            output_file.write(f"{signal}\t\t{offset}\t\t{bit}\n")
 
     print(f"信息已保存到：{output_filepath}")
 
@@ -38,10 +36,10 @@ def save_information_to_txt(signal_info, bit_info, row_info, output_filepath):
 excel_filepath = input("请输入Excel文件路径：")
 
 # 提取信息
-signal_info, bit_info, row_info = extract_signal_information(excel_filepath)
+signal_info, offset_info, bit_info = extract_signal_information(excel_filepath)
 
 # 指定输出文件路径
 output_filepath = 'output.txt'
 
 # 保存信息到TXT文件
-save_information_to_txt(signal_info, bit_info, row_info, output_filepath)
+save_information_to_txt(signal_info, offset_info, bit_info, output_filepath)
