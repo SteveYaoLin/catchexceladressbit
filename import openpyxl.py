507e525edd09eb5e06e7f4ee66a684a64e1af19d
SteveYaoLin <yaolin0616@163.com>
2023-06-25 11:36:40 +0800

    请再帮我继续修改。第1行是所有被检测元素的bit数，第1列是所有被检测元素的地址偏移量，所以这两部分不需要检测。例如OUTPUT.TXT中不需要出现“31		None		31”
    Signed-off-by: SteveYaoLin <yaolin0616@163.com>

--- import openpyxl.py
+++ import openpyxl.py
@@ -9,14 +9,14 @@ def extract_signal_information(filepath):
     offset_info = []
     bit_info = []
 
-    for row in range(1, 5):  # 假设地址偏移量在第2行到第5行之间
-        for col in range(1, 34):  # 假设bit数在第2列到第33列之间
+    for row in range(2, 6):  # 假设地址偏移量在第2行到第5行之间
+        for col in range(2, 34):  # 假设bit数在第2列到第33列之间
             signal = sheet.cell(row=row, column=col).value
             offset = sheet.cell(row=row, column=1).value
             bit = sheet.cell(row=1, column=col).value
 
             # 跳过空单元格和标题行
-            if signal is None or signal == "SIGNAL":
+            if signal is None or signal == "SIGNAL" or bit is None:
                 continue
 
             signal_info.append(signal)
