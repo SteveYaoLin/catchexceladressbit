214db0164e8fb2462cc1f5a4ad2f0216ce597358
SteveYaoLin <yaolin0616@163.com>
2023-07-25 12:33:45 +0800

    create v and h from efuse_disable

--- from_efuse_disable_2_h_v.py
+++ from_efuse_disable_2_h_v.py
@@ -17,36 +17,31 @@ def generate_v_file(item):
 
 if __name__ == "__main__":
     txt_file_path = input("请输入包含项的TXT文件的路径: ")
-    output_log_file = "extraction_log.txt"
+    output_v_file = "output.v"
+    output_h_file = "output.h"
+    log_file = "extraction_log.txt"
 
     # 读取TXT文件并提取项目
     extracted_items = extract_items_from_txt(txt_file_path)
 
-    # 生成.h和.v文件内容，并记录提取的文件路径和内容到LOG
-    with open(output_log_file, 'w') as log_file:
+    # 生成.v文件内容
+    with open(output_v_file, 'w') as v_file:
+        for item in extracted_items:
+            v_program = generate_v_file(item)
+            v_file.write(v_program)
+
+    # 生成.h文件内容
+    with open(output_h_file, 'w') as h_file:
         for item in extracted_items:
             h_program = generate_h_file(item)
-            v_program = generate_v_file(item)
+            h_file.write(h_program)
 
-            print(f"\n生成的.h文件内容 ({item}):")
-            print(h_program)
-
-            print(f"\n生成的.v文件内容 ({item}):")
-            print(v_program)
-
-            # 将生成的.h和.v内容写入对应的文件
-            h_file_path = f"{item}.h"
-            v_file_path = f"{item}.v"
-            with open(h_file_path, 'w') as h_file:
-                h_file.write(h_program)
-
-            with open(v_file_path, 'w') as v_file:
-                v_file.write(v_program)
-
-            # 记录提取的文件路径和内容到LOG
+    # 记录提取的文件路径和内容到LOG
+    with open(log_file, 'w') as log_file:
+        for item in extracted_items:
             log_file.write(f"Extracted from {txt_file_path} - Item: {item}\n")
-            log_file.write(f"{h_file_path}:\n{h_program}\n")
-            log_file.write(f"{v_file_path}:\n{v_program}\n")
+            log_file.write(f"{output_h_file}:\n{generate_h_file(item)}\n")
+            log_file.write(f"{output_v_file}:\n{generate_v_file(item)}\n")
             log_file.write("\n\n")
 
-    print("\n已生成.h和.v文件，并记录提取结果到LOG文件。")
+    print("已生成.v和.h文件，并记录提取结果到LOG文件。")
