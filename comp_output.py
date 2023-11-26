723aec1c0a1f85941f339f1e3242e4c4e9d1e82d
SteveYaoLin <yaolin0616@163.com>
2023-11-26 21:12:46 +0800

    改每行对比为逐行对比，符合预期功能

--- comp_output.py
+++ comp_output.py
@@ -10,20 +10,31 @@ def compare_files(file1, file2, output_file):
     with open(file2, 'r') as f2:
         file2_lines = f2.readlines()
 
+    # 去除行末的换行符
+    file1_lines = [line.rstrip() for line in file1_lines]
+    file2_lines = [line.rstrip() for line in file2_lines]
+
     # 比较文件内容，找出不同的行
-    different_lines = []
-    for i, (line1, line2) in enumerate(zip(file1_lines, file2_lines)):
-        if line1 != line2:
-            different_lines.append((i+1, line1.strip(), line2.strip()))
+    different_lines_file1 = []
+    different_lines_file2 = []
+
+    for line in file1_lines:
+        if line in file2_lines:
+            file2_lines.remove(line)
+        else:
+            different_lines_file1.append(line)
+
+    different_lines_file2 = file2_lines
 
     # 输出比较结果到文件
     with open(output_file, 'w') as output:
         output.write(f"比较结果：\n")
-        output.write(f"文件1 ({file1}) 与 文件2 ({file2}) 的不同行：\n")
-        output.write(f"{'行号':<8} {'文件1行内容':<20} {'文件2行内容':<20}\n")
-        output.write(f"{'-'*50}\n")
-        for line_num, line1, line2 in different_lines:
-            output.write(f"{line_num:<8} {line1:<20} {line2:<20}\n")
+        output.write(f"第1个文件 ({file1}) 的不同行：\n")
+        for line in different_lines_file1:
+            output.write(f"{line}\n")
+        output.write(f"\n第2个文件 ({file2}) 的不同行：\n")
+        for line in different_lines_file2:
+            output.write(f"{line}\n")
 
     print(f"比较结果已保存到文件：{output_file}")
 
